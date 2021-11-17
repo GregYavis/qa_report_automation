103af42441c61094f3204023f63b25d49266fdf9
Григорий Козлов <g.kozlov@fairtech.ru>
2021-11-17 08:11:30 +0300

    Правки к выборке статусов определяющих готовность релиза

--- monitor/atlassian_monitoring/release_processor.py
+++ monitor/atlassian_monitoring/release_processor.py
@@ -57,9 +57,9 @@ class ReleaseProcessor(AtlassianConfig):
 
     def release_ready_for_report(self, release_name: str):
         issues_in_release = Issue.objects.filter(release_name=release_name)
-        logger.info(issues_in_release)
+        logger.info(list(issues_in_release))
         ready_issues = Issue.objects.filter(release_name=release_name, issue_status__in=self.in_qa_states())
-        logger.info(ready_issues)
+        logger.info(list(ready_issues))
         return list(issues_in_release) == list(ready_issues)
 
     def monitor_issues_manual(self, release_name):
